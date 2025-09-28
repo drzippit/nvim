7d665adf6d7efba37566952116db0aaf106a5be9
Zippit <drzippit@gmail.com>
2025-09-28 12:00:34 +0200

    formatting and mini.pairs

--- init.lua
+++ init.lua
@@ -1,29 +1,29 @@
 -- load config before anything else
-require 'config.keybinds'
-require 'config.core'
+require("config.keybinds")
+require("config.core")
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup {
-  {
-    import = 'plugins',
-    change_detection = {
-      notify = false,
-    },
-  },
-}
+require("lazy").setup({
+	{
+		import = "plugins",
+		change_detection = {
+			notify = false,
+		},
+	},
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,22 +1,22 @@
 return {
-  'MagicDuck/grug-far.nvim',
-  -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
-  -- additional lazy config to defer loading is not really needed...
-  config = function()
-    -- optional setup call to override plugin options
-    -- alternatively you can set options with vim.g.grug_far = { ... }
-    require('grug-far').setup {
-      -- options, see Configuration section below
-      -- there are no required options atm
-    }
-  end,
-  keys = {
-    { '<leader>fr', ':GrugFar<CR>', desc = 'Grug-Far (Find and Replace)', silent = true },
-    {
-      '<leader>fR',
-      ":lua require('grug-far').open({ prefills = { search = vim.fn.expand('<cword>') } })<CR>",
-      desc = 'Grug-Far (Find and Replace) current word',
-      silent = true,
-    },
-  },
+	"MagicDuck/grug-far.nvim",
+	-- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+	-- additional lazy config to defer loading is not really needed...
+	config = function()
+		-- optional setup call to override plugin options
+		-- alternatively you can set options with vim.g.grug_far = { ... }
+		require("grug-far").setup({
+			-- options, see Configuration section below
+			-- there are no required options atm
+		})
+	end,
+	keys = {
+		{ "<leader>fr", ":GrugFar<CR>", desc = "Grug-Far (Find and Replace)", silent = true },
+		{
+			"<leader>fR",
+			":lua require('grug-far').open({ prefills = { search = vim.fn.expand('<cword>') } })<CR>",
+			desc = "Grug-Far (Find and Replace) current word",
+			silent = true,
+		},
+	},
 }
