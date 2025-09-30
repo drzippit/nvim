40c6f15ff4f224be4716312c3b1a2751fd891fc0
Zippit <drzippit@gmail.com>
2025-09-30 23:50:45 +0200

    add yanky

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,23 @@
+return {
+	"gbprod/yanky.nvim",
+	dependencies = { "folke/snacks.nvim" },
+	keys = {
+		{
+			"<leader>p",
+			function()
+				Snacks.picker.yanky()
+			end,
+			mode = { "n", "x" },
+			desc = "Open Yank History",
+		},
+	},
+	opts = {
+		vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)"),
+		vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)"),
+		vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)"),
+		vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)"),
+
+		vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)"),
+		vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)"),
+	},
+}
