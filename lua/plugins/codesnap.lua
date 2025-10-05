c4136aaef532960dc2e81572b5c5f4e5ec6e8f1d
Zippit <drzippit@gmail.com>
2025-10-05 13:42:47 +0200

    add keys to codesnap

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,6 +1,10 @@
 return {
 	"mistricky/codesnap.nvim",
-	build = "make",
+	build = "build",
+	keys = {
+		{ "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+		{ "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+	},
 	lazy = true,
 	config = function()
 		require("codesnap").setup({
