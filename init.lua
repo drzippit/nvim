16d122b3aefd5010d48f5956cc668abe0fc0f7c0
Zippit <drzippit@gmail.com>
2025-10-06 10:55:53 +0200

    add autoclose for snacks explorer

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- load config before anything else
 require("config.keybinds")
 require("config.core")
+require("config.autoclose_snackse")
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
