5482207ea4167007de714da975a3e22803f7af40
Zippit <drzippit@gmail.com>
2025-10-06 14:26:45 +0200

    add neorg config and conceal level
    also cleaned up core by removing commented out settings

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -2,7 +2,21 @@ return {
 	"nvim-neorg/neorg",
 	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 	version = "*", -- Pin Neorg to the latest stable release
-	config = true,
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							notes = "~/Documents/neorg/",
+						},
+					},
+				},
+			},
+		})
+	end,
 	keys = {
 		{
 			"<leader><space>",
